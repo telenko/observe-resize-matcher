454124382ec98747a2fbe7f1a448ed94bc1c45fc
mangolik931@gmail.com <mangolik931@gmail.com>
2019-03-11 22:32:57 +0200

    Fixed resize callback detail rectangular data

--- src/OnResizeMatcher.js
+++ src/OnResizeMatcher.js
@@ -7,15 +7,13 @@ class OnResizeMatcher {
 
     connectedCallback() {
         this[OBSERVER] = new ResizeObserver(entries => {
-            let mutationDetail = {};
             for (const entry of entries) {
                 if (entry.target !== this.element) {
                     return;
                 }
-                const { left, top, width, height } = entry.contentRect;
-                mutationDetail = Object.assign({}, mutationDetail, { left, top, width, height });
             }
-            const resizeEvent = new CustomEvent("resize", { bubbles: false, detail: mutationDetail });
+            const detail = this.element.getBoundingClientRect();//@TODO use ResizeObserver data instead
+            const resizeEvent = new CustomEvent("resize", { bubbles: false, detail });
             this.element.dispatchEvent(resizeEvent);
         });
         this[OBSERVER].observe(this.element)
